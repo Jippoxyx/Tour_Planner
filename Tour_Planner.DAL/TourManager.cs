632c0db826f332291d0094b1d428d987117fb094
Jessica Görög <Jessica.Goeroeg@gmail.com>
2022-04-26 10:00:52 +0200

    started with MockData

--- Tour_Planner.DAL/TourManager.cs
+++ Tour_Planner.DAL/TourManager.cs
@@ -3,27 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tour_Planner.Models;
 
 namespace Tour_Planner.DAL
 {
     class TourManager : ITourManager
     {
-        public void AddTour()
+        public void AddTour(Tour tour)
         {
             throw new NotImplementedException();
         }
 
-        public void DeleteAllTours()
+
+        public void DeleteTour(Tour tour)
         {
             throw new NotImplementedException();
         }
 
-        public void DeleteTour()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateTours()
+        public void UpdateTours(Tour tour)
         {
             throw new NotImplementedException();
         }
